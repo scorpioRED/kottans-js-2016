2966f1fa93e21836690a3e9cdd8f659e28c11e10
scorpiored88 <scorpio.red.88@gmail.com>
2016-05-21 16:26:16 +0300

    second week,task #2

---
+++ week_01/posthtml/README.md
@@ -0,0 +1,5 @@
+Завдання №2
+put your index.html in posthtml project folder and run it by "npmt start" 
+Закинути свій index.html в папку posthtml та запустит скрипт шляхом "npm start"
+
+
---
+++ week_01/posthtml/index.js
@@ -0,0 +1,47 @@
+const PostHTML = require('posthtml');
+fs = require('fs');
+const html = fs.readFileSync('index.html','utf-8');
+let text = fs.readFileSync('bootstrapClasses','utf8').replace(/\n/g,'').split('.');
+let bootstrapClsses = text.slice(1);
+
+const pat = /js-/;
+
+const ifOne = (tmp,node,elClass) =>{
+    if(tmp.length == 1){
+        delete node.attrs.class
+    }else {
+        node.attrs.class = node.attrs.class.replace(elClass,'').replace(' ', '')
+    }
+}
+
+
+const plugin = inHtml =>
+    inHtml
+        .match({ attrs: true}, node =>{
+
+        if(node.attrs.class){
+            let tmp = node.attrs.class.split(' ');
+            tmp.forEach( elClass => {
+                if(bootstrapClsses.indexOf(elClass) != -1){
+                    ifOne(tmp,node,elClass);
+                }else {
+                    if(pat.test(elClass)){
+                        node.attrs['data-js'] = elClass.split('-')[1];
+                        ifOne(tmp,node,elClass)
+                    }
+                }
+            })
+        }
+
+        return node
+
+    })
+
+
+PostHTML([ plugin ])
+    .process(html)
+    .then(result =>
+    {
+        console.log(result.html)
+    })
+    .catch(console.error)
